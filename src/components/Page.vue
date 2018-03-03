8170ee4697945539e76c4c2d55ae8a4c32e00b5e
Igor Trofimov <igortr333@gmail.com>
2018-03-03 15:41:23 +1000

    page

---
+++ src/components/Page.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <div class="page">
+    <h1>{{title}}</h1>
+    <div class="page-body" v-html="content"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "page",
+    data() {
+      return storage.data.pages.find(page => page.path == this.$route.params.path)
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
